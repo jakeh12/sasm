dc7347cf8e34439a8d59d9da87c3fa930e575e80
Jakub Hladik <jakeh12@me.com>
2019-02-12 20:24:39 -0600

    parsing seems to be working

--- sasm/main.c
+++ sasm/main.c
@@ -3,47 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LABELS 65535
-#define MAX_INST 65535
-
-typedef struct {
-    int address;
-    char name[32];
-} label_t;
-
-
-typedef struct {
-    int address;
-    char a[32];
-    char b[32];
-    char c[32];
-} inst_t;
-
-int find_label(char* label, label_t* labels, int label_count)
-{
-    int i;
-    for (i = 0; i < label_count; i++)
-    {
-        if (strcmp(labels[i].name, label) == 0)
-            return labels[i].address;
-    }
-    return -1;
-}
-
 int main(int argc, const char * argv[]) {
     
     char* prog = "/Users/jhladik/Documents/Development/sasm/program.sasm";
     
-    // label table
-    label_t* labels = (label_t*) malloc(MAX_LABELS*sizeof(label_t));
-    int label_count = 0;
-    
-    // program table
-    inst_t* insts = (inst_t*) malloc(MAX_INST*sizeof(inst_t));
-    int inst_count = 0;
-    
-    uint8_t program[MAX_INST];
-    int program_byte_count = 0;
+
     
     // open file
     FILE* fp = fopen(prog, "r");
@@ -51,120 +15,58 @@ int main(int argc, const char * argv[]) {
     
     // read line by line
     char line[1000];
+    char* token;
+    char* args[100];
+    int args_count;
     while(fgets(line, sizeof(line), fp))
     {
-        // remove new line character
-        line[strlen(line)-1] = '\0';
         
-        // skip empty lines or comment lines
-        if (line[0] == ';' || line[0] == '\n' || line[0] == '\0')
+        // remove new line character
+        //line[strlen(line)-1] = '\0';
+        
+        // remove comments
+        char* cp = strchr(line, ';');
+        if (cp) *cp = '\0';
+        
+        // skip empty or comment lines
+        if (line[0] == '\0' || line[0] == ';' || (token = strtok(line, " ,\t\n\v\f")) == NULL)
             continue;
         
-        // check for label
-        if (line[strlen(line)-1] == ':')
+        // first token already parsed, use it
+        if(token[strlen(token)-1] == ':')
         {
-            // get rid off the colon
-            line[strlen(line)-1] = '\0';
+            // line has a label
+            token[strlen(token)-1] = '\0';
+            printf("LABEL %s\n", token);
             
-            labels[label_count].address = inst_count;
-            strcpy(labels[label_count].name, line);
-            label_count++;
-            // TODO: PUT LABEL IN THE TABLE
-            printf("LABEL: %s @ 0x%04X\n", line, inst_count);
+            // TODO: add label to table
+            
+            token = strtok(NULL, " ,\t\n\v\f");
+            if (!token) continue;
         }
-        else
-        {
-            inst_count++;
-        }
-    }
-    
-    printf("found %d labels\n", label_count);
-    
-    // second pass
-    inst_count = 0;
-    fseek(fp, 0, SEEK_SET);
-    while(fgets(line, sizeof(line), fp))
-    {
-        // remove new line character
-        line[strlen(line)-1] = '\0';
         
-        if (line[0]              == ';'  ||
-            line[0]              == '\n' ||
-            line[0]              == '\0' ||
-            line[strlen(line)-1] == ':')
+        // parse directive
+        printf("  DIRECTIVE %s ", token);
+        
+        // parse args
+        args_count = 0;
+        token = strtok(NULL, " ,\t\n\v\f");
+        
+        while (token)
         {
-            continue;
+            args[args_count++] = token;
+            token = strtok(NULL, " ,\t\n\v\f");
         }
-        else
+        
+        printf("(%d)\n", args_count);
+        
+        int i;
+        for (i = 0; i < args_count; i++)
         {
-            // TODO: ASSEMBLE LINE
-            printf("ASSEMBLING: %s @ 0x%04X\n", line, inst_count);
-            char* token = strtok(line, " ,\t\n\v\f");
-            
-            if (strcmp(token, "sble") == 0)
-            {
-                int label_addr;
-                
-                // pointer a
-                token = strtok(NULL, " ,\t\n\v\f");
-                label_addr = 6*find_label(token, labels, label_count);
-                program[program_byte_count] = label_addr & 0xFF;
-                program[program_byte_count+1] = (label_addr >> 8) & 0xFF;
-                program_byte_count += 2;
-                
-                // pointer b
-                token = strtok(NULL, " ,\t\n\v\f");
-                label_addr = 6*find_label(token, labels, label_count);
-                program[program_byte_count] = label_addr & 0xFF;
-                program[program_byte_count+1] = (label_addr >> 8) & 0xFF;
-                program_byte_count += 2;
-                
-                // pointer c
-                token = strtok(NULL, " ,\t\n\v\f");
-                if (token)
-                {
-                    label_addr = 6*find_label(token, labels, label_count);
-                    program[program_byte_count] = label_addr & 0xFF;
-                    program[program_byte_count+1] = (label_addr >> 8) & 0xFF;
-                    
-                }
-                else
-                {
-                    // c missing, use next instruction
-                    //program[program_byte_count] = inst_count + 1;
-                    program[program_byte_count] = 6*inst_count & 0xFF;
-                    program[program_byte_count+1] = (6*inst_count >> 8) & 0xFF;
-                }
-                program_byte_count += 2;
-                inst_count++;
-            }
-            else if (strcmp(token, "db") == 0)
-            {
-                // data byte
-                token = strtok(NULL, " ,\t\n\v\f");
-                uint8_t value = (uint8_t) strtol(token, NULL, 10);
-                program[program_byte_count] = value;
-                program_byte_count++;
-                //inst_count++;
-            }
-            else
-            {
-                printf("error parsing token\n");
-                exit(EXIT_FAILURE);
-            }
+            printf("    ARGUMENT %s\n", args[i]);
         }
         
     }
-    
-    int i;
-    for (i = 0; i < program_byte_count; i++)
-    {
-        printf("%04x: %02x\n", i, program[i]);
-    }
-    printf("\n");
-    
-    free(labels);
-    free(insts);
     
     fclose(fp);
     exit(EXIT_SUCCESS);
