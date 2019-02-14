ea2574a4bf7f3a4789cbafca7be157802f654ba0
Jakub Hladik <jakeh12@me.com>
2019-02-13 19:20:56 -0600

    doesn't work

--- sasm/main.c
+++ sasm/main.c
@@ -6,16 +6,17 @@
 typedef struct
 {
     int addr;
-    char dir[32];
-    char* args[100];
-    int args_count;
-} line_t;
+    char* dir;
+    char* args[3];
+}
+line_t;
 
 typedef struct
 {
     int addr;
-    char name[32];
-} label_t;
+    char* name;
+}
+label_t;
 
 
 int find_label_addr(char* name, label_t* labels, int label_count)
@@ -49,8 +50,7 @@ int main(int argc, const char * argv[]) {
     // read line by line
     char line[1000];
     char* token;
-    char* args[100];
-    int args_count;
+    int arg_count = 0;
     int lc = 0;
     while(fgets(line, sizeof(line), fp))
     {
@@ -68,19 +68,15 @@ int main(int argc, const char * argv[]) {
         {
             // line has a label
             token[strlen(token)-1] = '\0';
-            //printf("LABEL %s @ 0x%04x\n", token, lc);
-            
-            // TODO: add label to table
             labels[label_count].addr = lc;
-            strcpy(labels[label_count].name, token);
+            labels[label_count].name = token;
             label_count++;
-            
             token = strtok(NULL, " ,\t\n\v\f");
             if (!token) continue;
         }
         
         lines[line_count].addr = lc;
-        strcpy(lines[line_count].dir, token);
+        lines[line_count].dir = token;
         
         // parse directive
         if (strcmp(token, "sble") == 0)
@@ -96,39 +92,19 @@ int main(int argc, const char * argv[]) {
             printf("ERROR: UNKNOWN DIRECTIVE\n");
             exit(EXIT_FAILURE);
         }
-        //printf("  DIRECTIVE %s ", token);
         
         // parse args
-        args_count = 0;
+        arg_count = 0;
         token = strtok(NULL, " ,\t\n\v\f");
         
         while (token)
         {
-            char* arg = (char*) calloc(32, sizeof(char));
-            strcpy(arg, token);
-            lines[line_count].args[args_count] = arg;
-            args[args_count++] = token;
+            lines[line_count].args[arg_count] = token;
+            arg_count++;
             token = strtok(NULL, " ,\t\n\v\f");
         }
-        
-        lines[line_count].args_count = args_count;
-        
-        //printf("(%d)\n", args_count);
-        
-        int i;
-        for (i = 0; i < args_count; i++)
-        {
-            
-            //printf("    ARGUMENT %s\n", args[i]);
-        }
-        
         line_count++;
-        
     }
-    
-    
-    printf("listing:\n");
-    
     
     int i;
     for (i = 0; i < line_count; i++)
@@ -136,78 +112,55 @@ int main(int argc, const char * argv[]) {
         
         if (strcmp(lines[i].dir, "sble") == 0)
         {
-            printf("0x%04x\t%s\t", lines[i].addr, lines[i].dir);
-            
-            int addr = find_label_addr(lines[i].args[0], labels, label_count);
-            if (addr == -1)
+            int a_addr = find_label_addr(lines[i].args[0], labels, label_count);
+            if (a_addr == -1)
             {
                 printf("ERROR: LABEL DOES NOT EXIST\n");
                 exit(EXIT_FAILURE);
             }
-            program[pc] = addr & 0xFF;
+            program[pc] = a_addr & 0xFF;
             pc++;
-            program[pc] = (addr >> 8) & 0xFF;
+            program[pc] = (a_addr >> 8) & 0xFF;
             pc++;
             
-            addr = find_label_addr(lines[i].args[1], labels, label_count);
-            if (addr == -1)
+            int b_addr = find_label_addr(lines[i].args[1], labels, label_count);
+            if (b_addr == -1)
             {
                 printf("ERROR: LABEL DOES NOT EXIST\n");
                 exit(EXIT_FAILURE);
             }
-            program[pc] = addr & 0xFF;
+            program[pc] = b_addr & 0xFF;
             pc++;
-            program[pc] = (addr >> 8) & 0xFF;
+            program[pc] = (b_addr >> 8) & 0xFF;
             pc++;
             
-            if (lines[i].args_count == 2)
+            int c_addr;
+            if (lines[i].args[2])
             {
-                program[pc] = (pc + 2) & 0xFF;
-                pc++;
-                program[pc] = ((pc + 2) >> 8) & 0xFF;
-                pc++;
-            }
-            else
-            {
-                addr = find_label_addr(lines[i].args[2], labels, label_count);
-                if (addr == -1)
+                c_addr = find_label_addr(lines[i].args[0], labels, label_count);
+                if (c_addr == -1)
                 {
                     printf("ERROR: LABEL DOES NOT EXIST\n");
                     exit(EXIT_FAILURE);
                 }
-                program[pc] = addr & 0xFF;
-                pc++;
-                program[pc] = (addr >> 8) & 0xFF;
-                pc++;
             }
-            printf("\n");
+            else
+            {
+                c_addr = pc + 2;
+            }
+            program[pc] = c_addr & 0xFF;
+            pc++;
+            program[pc] = (c_addr >> 8) & 0xFF;
+            pc++;
         }
         else if (strcmp(lines[i].dir, "db") == 0)
         {
             // parse constants
-            uint8_t c = (uint8_t) strtol(lines[i].args[0], NULL, 10);
-            program[pc] = c;
+            program[pc] = (uint8_t) strtol(lines[i].args[0], NULL, 10);;
             pc++;
-            
-            
-            
-            printf("0x%04x\t%s\t", lines[i].addr, lines[i].dir);
-            int j;
-            for (j = 0; j < lines[i].args_count; j++)
-            {
-                printf("%s\t", lines[i].args[j]);
-            }
-            printf("\n");
         }
     }
     
-    printf("\nlables:\n");
-
-    for (i = 0; i < label_count; i++)
-    {
-        printf("0x%04x\t%s\n", labels[i].addr, labels[i].name);
-    }
-
     printf("\ntranslated:\n");
     for (i = 0; i < pc; i++)
     {
